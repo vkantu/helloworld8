1e03d68ea3e32226c3b5b869d96f106043648e4b
vkantu <99831624+vkantu@users.noreply.github.com>
2023-04-01 21:13:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 8080;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Congratulations on your Node Hello World Application built on Docker!');
+  res.send('Congratulations on your Node Hello World Application built on Docker! Github is using to pull and push the commit easitly at a time');
 })
 
 app.listen(port, host);
